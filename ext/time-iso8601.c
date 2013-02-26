fcf26f65d2d6fb7fdd5c6a8e4eb0f5022a2b6943
Ryan Tomayko <rtomayko@gmail.com>
2013-02-26 02:29:08 -0800

    First working version of iso8601_strict

--- ext/time-iso8601.c
+++ ext/time-iso8601.c
@@ -13,35 +13,112 @@
 static ID id_iso8601;         /* :iso8601 */
 static ID id_iso8601_strict;  /* :iso8601_strict */
 static ID id_new;             /* :new */
-
-static VALUE T;               /* "T" */
-static VALUE Z;               /* "Z" */
+static ID id_utc;             /* :utc */
+static ID id_mktime;          /* :mktime */
 
 static VALUE
-rb_time_iso8601_strict(int argc, VALUE * argv, VALUE self)
+time_iso8601_strict(char * str, int len)
 {
-  return Qnil;
+	VALUE time = Qnil;
+	char * ps = str;
+	char * pe;
+	int year, mon, day, hour, min, sec, tz;
+
+	year = strtol(ps, &pe, 10);
+	if (pe > ps && *pe == '-')
+	{
+		ps = pe + 1;
+		mon = strtol(ps, &pe, 10);
+		if (pe > ps && *pe == '-' && mon >= 1 && mon <= 12)
+		{
+			ps = pe + 1;
+			day = strtol(ps, &pe, 10);
+			if (pe > ps && *pe == 'T' && day >= 1 && day <= 31)
+			{
+				ps = pe + 1;
+				hour = strtol(ps, &pe, 10);
+				if (pe > ps && *pe == ':' && hour >= 0 && hour <= 24)
+				{
+					ps = pe + 1;
+					min = strtol(ps, &pe, 10);
+					if (pe > ps && *pe == ':' && min >= 0 && min <= 61)
+					{
+						ps = pe + 1;
+						sec = strtol(ps, &pe, 10);
+						if (pe > ps && sec >= 0 && sec <= 61)
+						{
+							if (pe >= (str + len))
+							{
+								/* no timezone, use local time */
+								time = rb_funcall(rb_cTime, id_mktime, 6,
+								                  INT2FIX(year), INT2FIX(mon), INT2FIX(day),
+								                  INT2FIX(hour), INT2FIX(min), INT2FIX(sec));
+							}
+							else
+							{
+								ps = pe;
+								switch(*ps)
+								{
+									case 'Z': /* utc timezone */
+										time = rb_funcall(rb_cTime, id_utc, 6,
+										                  INT2FIX(year), INT2FIX(mon), INT2FIX(day),
+										                  INT2FIX(hour), INT2FIX(min), INT2FIX(sec));
+										break;
+
+									case '+':
+									case '-':
+										time = rb_funcall(rb_cTime, id_new, 7,
+										                  INT2FIX(year), INT2FIX(mon), INT2FIX(day),
+										                  INT2FIX(hour), INT2FIX(min), INT2FIX(sec),
+										                  rb_str_new(ps, len - (ps - str)));
+										break;
+
+									default:
+										return Qnil;
+								}
+							}
+
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return time;
+}
+
+static VALUE
+rb_time_iso8601_strict(VALUE self, VALUE str)
+{
+	/* minumum possible ISO8601 strict time value */
+	if (RSTRING_LEN(str) < 16)
+		rb_raise(rb_eArgError, "invalid date: %p", (void*)str);
+
+	VALUE time = time_iso8601_strict(StringValueCStr(str), RSTRING_LEN(str));
+
+	if (NIL_P(time))
+		rb_raise(rb_eArgError, "invalid date: %p", (void*)str);
+
+	return time;
 }
 
 VALUE
 Init_time_iso8601() {
-    /* bring in time.rb and time.c */
-    rb_require("time");
+	/* bring in time.rb and time.c */
+	rb_require("time");
 
-    /* Time class methods */
-    rb_define_singleton_method(rb_cTime, "iso8601_strict", rb_time_iso8601_strict, -1);
+	/* Time class methods */
+	rb_define_singleton_method(rb_cTime, "iso8601_strict", rb_time_iso8601_strict, 1);
 
-    /* Token Strings */
-    Z = rb_str_new2("Z");
-    rb_gc_register_address(&Z);
+	/* symbols */
+	id_new = rb_intern("new");
+	id_mktime = rb_intern("mktime");
+	id_utc = rb_intern("utc");
+	id_iso8601 = rb_intern("iso8601");
+	id_iso8601_strict = rb_intern("iso8601_strict");
 
-    T = rb_str_new2("T");
-    rb_gc_register_address(&T);
-
-    /* Symbol Constants */
-    id_new = rb_intern("new");
-    id_iso8601 = rb_intern("iso8601");
-    id_iso8601_strict = rb_intern("iso8601_strict");
-
-    return Qnil;
+	return Qnil;
 }
+
+/* vim: set noexpandtab: */
