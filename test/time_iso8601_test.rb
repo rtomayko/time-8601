6d738f2669d82d1f62fccd76f7e83117eaceb5ae
Ryan Tomayko <rtomayko@gmail.com>
2013-02-26 03:12:20 -0800

    Explicit zone tests

--- test/time_iso8601_test.rb
+++ test/time_iso8601_test.rb
@@ -41,4 +41,40 @@ class TimeISO8601Test < MiniTest::Unit::TestCase
     assert !time.utc?, "should not be UTC"
     assert_equal Time.new.zone, time.zone
   end
+
+  def test_explicit_zone_time_parsing
+    time = Time.iso8601_strict("2013-02-25T18:30:00-05:00")
+    assert_equal 2013, time.year
+    assert_equal 2, time.month
+    assert_equal 25, time.day
+    assert_equal 18, time.hour
+    assert_equal 30, time.min
+    assert_equal 0, time.sec
+    assert_equal -5 * 60 * 60, time.utc_offset
+  end
+
+  def test_explicit_zone_time_zone_info
+    time = Time.iso8601_strict("2013-02-25T18:30:00-05:00")
+    assert !time.utc?, "should not be UTC"
+  end
+
+  def test_explicit_zone_positive_offset
+    time = Time.iso8601_strict("2013-02-25T18:30:00+05:00")
+    assert_equal 5 * 60 * 60, time.utc_offset
+    assert !time.utc?, "should not be UTC"
+  end
+
+  def test_explicit_zone_minute_offset
+    time = Time.iso8601_strict("2013-02-25T18:30:00+05:27")
+    assert_equal (5 * 60 * 60) + 27 * 60, time.utc_offset
+    assert !time.utc?, "should not be UTC"
+  end
+
+  def test_explicit_zone_utc
+    time = Time.iso8601_strict("2013-02-25T18:30:00+00:00")
+    assert_equal 0, time.utc_offset
+    time.utc # TODO <- shouldn't need that
+    assert time.utc?, "should be UTC"
+    skip
+  end
 end
